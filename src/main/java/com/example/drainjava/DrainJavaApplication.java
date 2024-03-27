d56fc534521c621677ff82331629034e09f5a940
chong1ha <zxcvbnm9931@gmail.com>
2024-03-27 14:49:37 +0900

    Initial commit

---
+++ src/main/java/com/example/drainjava/DrainJavaApplication.java
@@ -0,0 +1,18 @@
+package com.example.drainjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrainJavaApplication {
+
+	/**
+	 * Framework : SpringBoot 2.7.6 (Spring 5.x)<br>
+	 * Build Tool : Maven<br>
+	 * JAVA 8
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(DrainJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/drainjava/DrainJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drainjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrainJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
