720da04eb5bfee3932bd723611c4c804476a12d1
chong1ha <zxcvbnm9931@gmail.com>
2024-04-08 09:52:49 +0900

    Update SimpleProfiler

--- src/main/java/com/example/drainjava/builtins/drain/SimpleProfiler.java
+++ src/main/java/com/example/drainjava/builtins/drain/SimpleProfiler.java
@@ -21,6 +21,11 @@ public class SimpleProfiler extends Profiler {
     /** 섹션별, 경과 시간 저장 */
     private Map<String, Long> elapsedTimes = new HashMap<>();
 
+    private Map<String, Long> cumulativeTimes = new HashMap<>();
+
+
+    private Map<String, Integer> callCount = new HashMap<>();
+
     /**
      * 섹션 시작 <br>
      *  : 나노초 단위 사용
@@ -45,8 +50,14 @@ public class SimpleProfiler extends Profiler {
         // 경과 시간 측정
         long elapsedTime = endTime - startTime;
 
+        // 메소드 개별 시간 저장
+        elapsedTimes.put(sectionName, elapsedTime);
         // 섹션별, 누적 시간 저장 (동일 섹션)
-        elapsedTimes.put(sectionName, elapsedTimes.getOrDefault(sectionName, 0L) + elapsedTime);
+        cumulativeTimes.put(sectionName, cumulativeTimes.getOrDefault(sectionName, 0L) + elapsedTime);
+        // 섹션별, 누적 카운트
+        callCount.put(sectionName, callCount.getOrDefault(sectionName, 0) + 1);
+        // 시작시간 초기화
+        startTimes.remove(sectionName);
     }
 
     /**
@@ -55,11 +66,22 @@ public class SimpleProfiler extends Profiler {
     @Override
     public void report() {
         for (Map.Entry<String, Long> entry : elapsedTimes.entrySet()) {
+            // 개별 (나노초 --> 초 변환)
             String sectionName = entry.getKey();
             long elapsedTimeInNano = entry.getValue();
-            // 나노초 --> 초 변환
             double elapsedTimeInSec = (double) elapsedTimeInNano / 1000000000.0;
-            System.out.printf("Section '%s' took %.4f seconds.%n", sectionName, elapsedTimeInSec);
+
+            // 누적 (나노초 --> 초 변환)
+            long cumulativeTimeInNano = cumulativeTimes.getOrDefault(sectionName, 0L);
+            double cumulativeTimeInSec = (double) cumulativeTimeInNano / 1000000000.0;
+
+            // 메시지 갯수 카운트
+            int count = callCount.getOrDefault(sectionName, 0);
+            // 1건당 처리
+            double averageTimePerCall = cumulativeTimeInSec / count;
+            System.out.printf("Section '%s' took %.8f seconds (Total: %.8f seconds, CallCount: %d, Average Time Per Call: %.8f seconds)%n",
+                    sectionName, elapsedTimeInSec, cumulativeTimeInSec, count, averageTimePerCall);
+
         }
     }
 
@@ -69,6 +91,7 @@ public class SimpleProfiler extends Profiler {
      * @param sectionName 섹션 이름
      * @param method 실행할 메소드
      */
+    @Override
     public void executeWithProfiling(String sectionName, Runnable method) {
         startSection(sectionName);
         method.run();
