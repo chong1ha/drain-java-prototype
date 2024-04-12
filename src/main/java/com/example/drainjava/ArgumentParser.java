ce72df39272a3c050ea0ca91842c110df0347acd
chong1ha <zxcvbnm9931@gmail.com>
2024-04-12 10:39:59 +0900

    Fix ArgumentParser

--- src/main/java/com/example/drainjava/ArgumentParser.java
+++ src/main/java/com/example/drainjava/ArgumentParser.java
@@ -4,7 +4,6 @@ import com.example.drainjava.builtins.FilePersistence;
 import com.example.drainjava.builtins.TemplateMiner;
 import com.example.drainjava.builtins.drain.LogCluster;
 import com.example.drainjava.common.CommonUtil;
-import com.example.drainjava.common.Pair;
 import com.example.drainjava.common.util.StringUtil;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +16,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -62,7 +62,6 @@ public class ArgumentParser implements ApplicationRunner {
 
         // 옵션 1. 추론모드 선택 여부
         boolean inferenceMode = args.containsOption("i") || args.containsOption("-infer");
-
         //옵션 1. 학습모드 선택 여부
         boolean trainingMode = args.containsOption("t") || args.containsOption("-train");
 
@@ -73,103 +72,129 @@ public class ArgumentParser implements ApplicationRunner {
             return;
         }
 
+
         // 옵션 3. 애플리케이션 버전 정보
         if (args.containsOption("v") || args.containsOption("-version")) {
             System.out.println(displayVersion());
             return;
         }
 
+
         // 옵션 4. Drain 수행 전, 로그 포맷을 기준으로 해당 로그메시지 전처리
-        List<String> headers = new ArrayList<>();
         Pattern regex = null;
         if (args.containsOption("l") || args.containsOption("-log-format")) {
-            List<String> optionValues = args.getOptionValues("l");
-            String logFormat = optionValues.get(0);
 
-            Pair<Pattern, List<String>> result = commonUtil.generateLogFormatRegex(logFormat, headers);
-            regex = result.getFirst();
-            headers = result.getSecond();
+            Optional<String> logFormatOption = Optional.ofNullable(
+                    args.containsOption("-log-format") ? args.getOptionValues("-log-format").get(0) : args.getOptionValues("l").get(0)
+            );
+
+            if (logFormatOption.isPresent() == true) {
+                String logFormat = logFormatOption.get();
+
+                // 로그 포맷 - 정규표현식 생성
+                regex = commonUtil.generateLogFormatRegex(logFormat, new ArrayList<>()).getFirst();
+            }
         }
 
-        // 스냅샷 기능이 활성화 상태라면, j, k, r 중 하나라도 선택되어야 함
-        if ((inferenceMode || trainingMode) && !args.containsOption("f") && !args.containsOption("k") && !args.containsOption("r")) {
-            System.out.println(displayUsage());
-            System.out.println("Error: f, k, r 중 하나를 선택해야 합니다.");
-            return;
-        }
 
         // 학습모드 (현재 X)
-        if (trainingMode) {
+        if (trainingMode == true) {
             return;
         }
 
         // 모드, 둘 중 하나는 선택
-        if (!inferenceMode && !trainingMode) {
+        if ((inferenceMode == false) && (trainingMode == false)) {
             System.out.println(displayUsage());
-            System.out.println("Error: 추론 모드 또는 학습 모드를 선택해야 합니다.");
+            log.warn("ARGS: Choose Inference or Training Mode");
             return;
         }
 
         // 모드, 동시 선택 X
-        if (inferenceMode && trainingMode) {
+        if ((inferenceMode == true) && (trainingMode == true)) {
             System.out.println(displayUsage());
-            System.out.println("Error: 추론 모드와 학습 모드는 동시에 사용할 수 없습니다.");
+            log.warn("ARGS: Only One Choose (Not all)");
             return;
         }
+
 
         // 옵션 5. 상태정보 저장 및 로드, JSON 파일
         String binFilePath = "";
         if (args.containsOption("f") || args.containsOption("-file")) {
-            List<String> optionValues = args.getOptionValues("f");
-            if (optionValues == null || optionValues.isEmpty()) {
-                System.out.println("Error: 파일이 비어있습니다.");
+
+            Optional<String> binFilePathOption = Optional.ofNullable(
+                    args.containsOption("-file") ? args.getOptionValues("-file").get(0) : args.getOptionValues("f").get(0)
+            );
+
+            if (binFilePathOption.isPresent() == false) {
+                log.warn("ARGS: Missing Snapshot Binary File");
                 return;
             }
-
-            binFilePath = optionValues.get(0);
+            binFilePath = binFilePathOption.get();
         }
+
 
         // 옵션 6. 상태정보 저장 및 로드, Kafka
         if (args.containsOption("k") || args.containsOption("-kafka")) {
             return;
         }
 
+
         // 옵션 7. 상태정보 저장 및 로드, Redis
         if (args.containsOption("r") || args.containsOption("-redis")) {
             return;
         }
+
+
+        // 스냅샷 기능 (j, k, r) 중 하나라도 선택되어야 함
+        if (!args.containsOption("f") && !args.containsOption("k") &&
+                !args.containsOption("r") && !args.containsOption("-file") &&
+                !args.containsOption("-kafka") && !args.containsOption("-redis")
+        ) {
+            System.out.println(displayUsage());
+            log.warn("ARGS: At least one of f, k, r must be selected");
+            return;
+        }
+
 
         // 옵션 8. 세부정보 출력
         if (args.containsOption("-verbose")) {
             return;
         }
 
+
         // 옵션 9. 드레인 설정파일 (.ini)
         String drainIniFilePath = "";
-        if (args.containsOption("e")) {
-            drainIniFilePath = args.getOptionValues("e").get(0);
+        if (args.containsOption("e") || args.containsOption("-env")) {
 
-            // 예외처리
-            if (StringUtil.isEmpty(drainIniFilePath)) {
-                System.out.println("Error: drain3.ini 파일이 비어있습니다.");
+            Optional<String> drainIniOption = Optional.ofNullable(
+                    args.containsOption("-env") ? args.getOptionValues("-env").get(0) : args.getOptionValues("e").get(0)
+            );
+
+            if (drainIniOption.isPresent() == false) {
+                log.warn("ARGS: Missing Environment ini File");
                 return;
             }
+            drainIniFilePath = drainIniOption.get();
         }
+
 
         // FILE 인자들 (로그데이터 파일들, Drain 입력데이터)
         List<String> logFIlePathList = args.getNonOptionArgs();
-        if (logFIlePathList.isEmpty() == true) {
+        if (logFIlePathList.isEmpty() == true || logFIlePathList == null) {
             System.out.println(displayUsage());
-            System.out.println("Error: FILE 인자 누락되었습니다.");
+            log.warn("ARGS: Missing Log data File");
             return;
         }
 
-        // 전체 프로세스 실행
+
+        // (match) 전체 프로세스 실행
         filePersistence.setFilePath(binFilePath);
         templateMiner.init(drainIniFilePath);
         templateMiner.loadState();
 
-        int fileCount = 0;
+        // 실행시간 측정
+        long startTime = System.currentTimeMillis();
+
         for (String logFilePath : logFIlePathList) {
 
             // 예외처리
@@ -182,7 +207,6 @@ public class ArgumentParser implements ApplicationRunner {
                 throw new IllegalArgumentException("The file must have a .log extension.");
             }
 
-            int noTemplateCount = 0;
             // 개별 (xxx.log) 로그 파일 처리
             try (BufferedReader br = new BufferedReader(new FileReader(logFilePath))) {
 
@@ -201,24 +225,18 @@ public class ArgumentParser implements ApplicationRunner {
                     } else {
                         line = line.substring(line.indexOf(": ") + 2);
                     }
-                    System.out.println("----------------------------");
                     // 매칭
                     LogCluster cluster = templateMiner.match(line, "never");
 
-                    System.out.println("# INPUT: " + line);
-                    if (cluster != null) {
-                        System.out.println("# OUTPUT: " + cluster.getTemplate());
-                    } else {
-                        System.out.println("# OUTPUT: No matching template found.");
-                        noTemplateCount += 1;
+                    if (cluster == null) {
+
                     }
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                log.error("Error reading log file: " + logFilePath, e);
             }
-            System.out.println("============================");
-            System.out.printf("No matching template: %d%n", noTemplateCount);
         }
+        log.info("Execution time: " + (System.currentTimeMillis() - startTime) + " milliseconds");
     }
 
     /**
@@ -236,7 +254,7 @@ public class ArgumentParser implements ApplicationRunner {
                 "  -l, --log-format\t\tuse formatting logs used for preprocessing\n" +
                 "  -i, --infer\t\tInference Mode\n" +
                 "  -t, --train\t\tTraining Mode\n" +
-                "  -e, --log-format\t\tdrain ini file path\n" +
+                "  -e, --env\t\tdrain ini file path\n" +
                 "  -f, --file\t\tspecify the bin file path for loading/storing the model state when using file option (inference mode:Loading / training:Storing)\n" +
                 "  -k, --kafka\t\tuse Kafka for loading/storing the model state (inference mode:Loading / training:Storing)\n" +
                 "  -r, --redis\t\tuse Redis for loading/storing the model state (inference mode:Loading / training:Storing)\n" +
@@ -254,7 +272,6 @@ public class ArgumentParser implements ApplicationRunner {
      * USAGE
      */
     private String displayUsage() {
-//        return "Usage: java -jar xx.jar [-hv] [--verbose] [-it] [-l=LOG_FORMAT_STRING] [-f=SNAPSHOT_BIN_PATH] [-r=REDIS_PATH] [-k=KAFKA TOPIC PATH] FILE";
-        return "Usage: java -jar xx.jar [-hv] [-it] [-f=SNAPSHOT_BIN_PATH] FILE";
+        return "Usage: java -jar xx.jar [-hv] [-i] [-l=LOG_FORMAT_STRING] [-e=DRAIN_INI_FILE_PATH] [-f=SNAPSHOT_BIN_PATH] FILE";
     }
 }
